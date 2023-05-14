222cfb50054bd56cad799f273354ad609d6dd2fa
Quackdratic <39701964+Quackdratic@users.noreply.github.com>
2023-05-14 19:33:03 +0300

    add image view to aa::VulkanRegistrar

--- src/vulkan/VulkanRegistrar.cpp
+++ src/vulkan/VulkanRegistrar.cpp
@@ -26,6 +26,8 @@ GLFWwindow*				 _glfwWindow;
 std::vector<VkImage>	 _swapChainImages;
 VkFormat				 _swapChainImageFormat;
 VkExtent2D				 _swapChainExtent;
+std::vector<VkImageView> _swapChainImageViews;
+
 
 
 const std::vector<const char*> _validationLayers = {
@@ -468,11 +470,39 @@ void _initSwapChain() {
 	}
 
 	vkGetSwapchainImagesKHR(_logicWorker, _swapChain, &imageCount, nullptr);
+	std::cout << "image count: " << imageCount << '\n';
 	_swapChainImages.resize(imageCount);
 	vkGetSwapchainImagesKHR(_logicWorker, _swapChain, &imageCount, _swapChainImages.data());
 
 	_swapChainImageFormat = vkSurfaceFormat.format;
 	_swapChainExtent = vkExtent;
+}
+
+void _initImageViews() {
+	_swapChainImageViews.resize(_swapChainImages.size());
+
+	for (size_t i = 0; i < _swapChainImages.size(); i++) {
+		VkImageViewCreateInfo createInfo { };
+		createInfo.sType							= VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+		createInfo.image							= _swapChainImages[i];
+		createInfo.viewType							= VK_IMAGE_VIEW_TYPE_2D;
+		createInfo.format							= _swapChainImageFormat;
+		createInfo.components.r						= VK_COMPONENT_SWIZZLE_IDENTITY;
+		createInfo.components.g						= VK_COMPONENT_SWIZZLE_IDENTITY;
+		createInfo.components.b						= VK_COMPONENT_SWIZZLE_IDENTITY;
+		createInfo.components.a						= VK_COMPONENT_SWIZZLE_IDENTITY;
+		createInfo.subresourceRange.aspectMask		= VK_IMAGE_ASPECT_COLOR_BIT;
+		createInfo.subresourceRange.baseMipLevel	= 0;
+		createInfo.subresourceRange.levelCount		= 1;
+		createInfo.subresourceRange.baseArrayLayer	= 0;
+		createInfo.subresourceRange.layerCount		= 1;
+
+		if (vkCreateImageView(
+			_logicWorker, &createInfo, nullptr, &_swapChainImageViews[i]
+		) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create image views!");
+		}
+	}
 }
 
 
@@ -483,9 +513,14 @@ void VulkanRegistrar::registerVulkan(GLFWwindow* window) {
 	_initPhysicalDevices();
 	_initLogicalDevice();
 	_initSwapChain();
+	_initImageViews();
 }
 
 void VulkanRegistrar::cleanVulkan() {
+	for (auto imageView : _swapChainImageViews) {
+		vkDestroyImageView(_logicWorker, imageView, nullptr);
+	}
+
 	vkDestroySwapchainKHR(_logicWorker, _swapChain, nullptr);
 
 	vkDestroyDevice(_logicWorker, nullptr);
